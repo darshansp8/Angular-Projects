443af95eda6d129df391eecd06e8bea0eefe656b
darshansp8 <d.sakpal38@gmail.com>
2020-04-20 01:18:57 +0530

    Added routing to the components

--- day2-assgn/src/app/app-routing.module.ts
+++ day2-assgn/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { EmployeeComponent } from './employee/employee.component';
+import { MyservicedemoComponent } from './myservicedemo/myservicedemo.component';
+import { PipedemoComponent } from './pipedemo/pipedemo.component';
+import { AlbumsComponent } from './HttpCrud/albums/albums.component';
+import { PhotosComponent } from './HttpCrud/photos/photos.component';
+import { PostsComponent } from './HttpCrud/posts/posts.component';
+import { UsersComponent } from './HttpCrud/users/users.component';
+import { AppComponent } from './app.component';
+ 
+  
+const routes: Routes = [
+  {path: "employeedemo", component: EmployeeComponent},
+  {path: "servicedemo", component: MyservicedemoComponent},
+  {path: "pipedemo", component: PipedemoComponent},
+  {path: "albums", component: AlbumsComponent},
+  {path: "photos", component: PhotosComponent},
+  {path: "posts", component: PostsComponent},
+  {path: "users", component: UsersComponent},
+  {path: "*", component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
