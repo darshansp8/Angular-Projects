c6517cab9f17a37ac88ccc62171494e0bd61f4dc
darshansp8 <d.sakpal38@gmail.com>
2020-06-11 00:06:56 +0530

    Assignment 2

---
+++ Udemy/basics-assignment-two/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DatabindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
